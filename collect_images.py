c5d442df9bcc1945270f81521d3e7fe6d05c7e7d
Eunchul Song <song9446@unist.ac.kr>
2019-08-10 20:02:10 +0900

    collect 10000

--- collect_images.py
+++ collect_images.py
@@ -10,13 +10,13 @@ import tool
 import urllib.parse
 
 TIMEOUT=3
-RETRY=3
+RETRY=10
 
 DATA_DIR = "data"
 IMAGE_DIR = os.path.join(DATA_DIR, "images")
 LABEL_PATH = os.path.join(DATA_DIR, "label.pkl")
 
-@tenacity.retry(retry=tenacity.retry_if_exception_type(asyncio.TimeoutError), stop=tenacity.stop_after_attempt(RETRY))
+@tenacity.retry(before_sleep=tool.log, stop=tenacity.stop_after_attempt(RETRY))
 async def fetch_img(url, sess):
     path = os.path.join(IMAGE_DIR, urllib.parse.quote(url, safe=""))
     async with sess.get(url) as res:
@@ -25,30 +25,36 @@ async def fetch_img(url, sess):
                 await f.write(data)
     return path
 
-@tenacity.retry(retry=tenacity.retry_if_exception_type(asyncio.TimeoutError), stop=tenacity.stop_after_attempt(RETRY))
+#@tenacity.retry(retry=(tenacity.retry_if_exception_type(asyncio.TimeoutError) | tenacity.retry_if_exception_type(aiohttp.ClientResponseError)), stop=tenacity.stop_after_attempt(RETRY))
+@tenacity.retry(before_sleep=tool.log, stop=tenacity.stop_after_attempt(RETRY))
 async def fetch(url, sess):
     async with sess.get(url) as res:
         r = await res.text()
-        print(r)
         parsed = lxml.html.fromstring(r)
+        title = parsed.xpath("//title")
+        if title and title[0].text and title[0].text.startswith("429"):
+            raise aiohttp.ClientResponseError
         tags = {ul.getprevious().text: [i.text for i in ul.xpath(".//a[@class='search-tag']")]
                 for ul in parsed.xpath("//aside/section[@id='tag-list']/ul")}
-        url = parsed.xpath("//section[@id='image-container']")[0].get("data-file-url")
+        cont = parsed.xpath("//section[@id='image-container']")
+        url = cont and cont[0].get("data-file-url")
         return (url, tags)
 
 async def record(url, sess, fp):
     try:
+        print(url)
         url, tags = await fetch(url, sess)
         if not url: return None
         path = await fetch_img(url, sess)
         pickle.dump((path, tags), fp)
     except Exception as e:
-        print(e)
+        raise(e)
+        print(e, url)
 
 
-async def collect(max_num=1):
+async def collect(max_num=1, last_num=0):
     conn = aiohttp.TCPConnector(
-            limit=32,
+            #limit=32,
             family=socket.AF_INET,
             verify_ssl=False,
             )
@@ -56,11 +62,23 @@ async def collect(max_num=1):
         async with aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(total=TIMEOUT)) as sess:
             return await tool.limited_api(
                     [record(f"https://danbooru.donmai.us/posts/{i}", sess, fp)
-                        for i in range(1,max_num+1)])
+                        for i in range(last_num+1,last_num+max_num+1)])
+
+def read_records():
+    with open(LABEL_PATH, "r+b") as fp:
+        while fp.read(1):
+            fp.seek(-1,1)
+            yield pickle.load(fp)
 
 def main():
+    #print(len([i for i in read_records()]))
+    #exit()
     loop = asyncio.get_event_loop()
-    print(loop.run_until_complete(collect(10)))
+    start = 12100
+    for i in range(10):
+        print(start + i*1000)
+        loop.run_until_complete(collect(max_num=1000, last_num=start+i*1000))
+        print(start + i*1000)
 
 if __name__ == "__main__":
     main()
--- tool.py
+++ tool.py
@@ -1,5 +1,5 @@
-THROUGHPUT=100
-TIME_UNIT=0.1
+THROUGHPUT=20
+TIME_UNIT=0.01
 import asyncio
 async def wrapper(coru, semaphore, sec):
     async with semaphore:
@@ -9,3 +9,14 @@ async def wrapper(coru, semaphore, sec):
 def limited_api(corus, n=THROUGHPUT, sec=TIME_UNIT):
     semaphore = asyncio.Semaphore(n)
     return asyncio.gather(*[wrapper(coru, semaphore, sec) for coru in corus])
+import logging
+logger = logging.getLogger(__name__)
+def log(retry_state):
+    if retry_state.attempt_number < 1:
+        loglevel = logging.INFO
+    else:
+        loglevel = logging.WARNING
+    logger.log(
+        loglevel, 'Retrying %s: attempt %s ended with: %s',
+        retry_state.fn, retry_state.attempt_number, retry_state.outcome)
+
